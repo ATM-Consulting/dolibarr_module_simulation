ea12e100cb27ba13ab63ea24fecdd6dbc350101e
phf <phf@atm-consulting.fr>
2016-05-20 11:56:13 +0200

    New extrafield sur tiers pour dire si leaser + ajout du filtre en plus sur l'extra sur fiche propal pour seléctionner le leaser

--- core/modules/modsimulation.class.php
+++ core/modules/modsimulation.class.php
@@ -294,9 +294,11 @@ class modsimulation extends DolibarrModules
 		
 		
 		$extra = new ExtraFields($this->db);
+		$extra->addExtraField('is_leaser', 'Est un leaser', 'select', 10, '', 'societe', 0, 0, '', unserialize('a:1:{s:7:"options";a:2:{i:0;s:3:"Non";i:1;s:3:"Oui";}}'));
+		
 		$extra->addExtraField('is_simulation', 'Simulation', 'select', 1, '', 'propal', 0, 0, '', unserialize('a:1:{s:7:"options";a:2:{i:0;s:3:"Non";i:1;s:3:"Oui";}}'));
 		
-		$extra->addExtraField('fk_leaser', 'Leaser', 'sellist', 2, '', 'propal', 0, 0, '', unserialize('a:1:{s:7:"options";a:1:{s:45:"societe:nom:rowid::fournisseur=1 AND status=1";N;}}'));
+		$extra->addExtraField('fk_leaser', 'Leaser', 'sellist', 2, '', 'propal', 0, 0, '', unserialize('a:1:{s:7:"options";a:1:{s:45:"societe:nom:rowid::fournisseur=1 AND status=1 AND extra.is_leaser=1";N;}}'));
 		$extra->addExtraField('default_nb_periode', 'Nombre de période par défaut', 'int', 3, 10, 'propal', 0, 0, '', unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'));
 		$extra->addExtraField('periodicite', 'Périodicité', 'select', 4, '', 'propal', 0, 0, '', unserialize('a:1:{s:7:"options";a:3:{i:0;s:13:"trimestrielle";i:1;s:9:"mensuelle";i:2;s:8:"annuelle";}}'));
 		$extra->addExtraField('coefficient', 'Coefficient ( en % )', 'double', 5, '10,4', 'propal', 0, 0, '', unserialize('a:1:{s:7:"options";a:1:{s:0:"";N;}}'));
